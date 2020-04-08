aa83e1fcba4bb62ef151a80945228e4e18a06335
CaydenPocrass <43300017+CaydenPocrass@users.noreply.github.com>
2020-04-08 14:21:04 -0700

    Encoder Vuforia IMU Localizer Created

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/MecanumLocalizerEVI.java
@@ -0,0 +1,85 @@
+package org.firstinspires.ftc.teamcode.drive;
+
+import android.util.Pair;
+
+import com.acmerobotics.roadrunner.drive.Drive;
+import com.acmerobotics.roadrunner.drive.MecanumDrive;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.kinematics.Kinematics;
+import com.acmerobotics.roadrunner.kinematics.MecanumKinematics;
+import com.acmerobotics.roadrunner.localization.Localizer;
+import com.acmerobotics.roadrunner.util.Angle;
+
+import org.firstinspires.ftc.teamcode.vision.IVuforiaEngine;
+import org.firstinspires.ftc.teamcode.vision.SkystoneVuforiaEngine;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.zip.GZIPInputStream;
+
+import static com.acmerobotics.roadrunner.drive.MecanumDrive.*;
+import static java.util.Collections.emptyList;
+import static org.firstinspires.ftc.teamcode.drive.DriveConstants.TRACK_WIDTH;
+import static org.firstinspires.ftc.teamcode.drive.DriveConstants.WHEEL_BASE;
+
+public class MecanumLocalizerEVI implements Localizer {
+    MecanumDrive drive;
+    SkystoneVuforiaEngine vuforia;
+    Pose2d initialPose;
+    public MecanumLocalizerEVI (MecanumDrive drive, SkystoneVuforiaEngine vuforia, Pose2d initialPose){
+       this.drive = drive;
+       this.vuforia = vuforia;
+       this.initialPose = initialPose;
+    }
+    Pose2d poseEstimate =  new Pose2d();
+    List<Double> lastWheelPositions = new ArrayList<>();
+    double lastExtHeading = Double.NaN;
+    Pose2d lastVPose = initialPose;
+    @Override
+    public void update() {
+    Pose2d fullDelta = new Pose2d();
+    List<Double> wheelPositions = drive.getWheelPositions();
+    double extHeading = drive.getExternalHeading();
+    if(!lastWheelPositions.isEmpty()){
+        List<Double> wheelDeltas = new ArrayList<Double>(wheelPositions.size());
+        for(int i = 0; i < wheelPositions.size(); i++) {
+            wheelDeltas.set(i, wheelPositions.get(i) - lastWheelPositions.get(i));
+        }
+        Pose2d robotPoseDelta = MecanumKinematics.wheelToRobotVelocities(wheelDeltas,TRACK_WIDTH,WHEEL_BASE);
+        double finalHeadingDelta = Angle.normDelta(extHeading-lastExtHeading);
+        Pose2d eiDelta = new Pose2d(robotPoseDelta.vec(),finalHeadingDelta);
+        Pair<Pose2d, Boolean> scan = vuforia.scanPose();
+        if(scan.second){
+            Pose2d vDelta = scan.first.minus(lastVPose);
+            fullDelta = eiDelta.plus(vDelta).div(2);
+            poseEstimate =
+                    Kinematics.relativeOdometryUpdate(poseEstimate,
+                            fullDelta);
+            lastVPose = scan.first;
+        }else {
+            poseEstimate =
+                    Kinematics.relativeOdometryUpdate(poseEstimate,
+                            new Pose2d(robotPoseDelta.vec(), finalHeadingDelta));
+            lastVPose = poseEstimate;
+        }
+        lastWheelPositions = wheelPositions;
+        lastExtHeading = extHeading;
+    }
+    }
+    @Override
+    public void setPoseEstimate(@NotNull Pose2d pose2d) {
+        lastWheelPositions = emptyList();
+        lastExtHeading = Double.NaN;
+        poseEstimate = pose2d;
+    }
+
+
+
+    @NotNull
+    @Override
+    public Pose2d getPoseEstimate() {
+        return poseEstimate;
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/IVuforiaEngine.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode.vision;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.vuforia.Vec3F;
+
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+
+public interface IVuforiaEngine {
+
+    void init(HardwareMap ahwMap);
+
+    OpenGLMatrix getPose(String id);
+
+    Vec3F getPosition(String id);
+
+    /**
+     *
+     * Gets the distance from the camera to a tracked object, in millimeters.
+     * If the object is not visible, this returns {@link Double#POSITIVE_INFINITY}<br/>
+     * <br/>
+     * Thanks to Mrs Shaw for helping us understand matrices.<br/>
+     * Thanks to <a href="https://youtu.be/vlb3P7arbkU?t=208">this video</a> for helping us understand homegenous transformation matrices.</br>
+     *
+     * @param id String id of a tracked object.
+     * @return distance in millimeters.
+     *
+     */
+    Double getDistanceToObject(String id);
+
+}
