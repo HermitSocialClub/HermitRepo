c5dec38922232ce7f97280cd57f3033e71511500
Arc-blroth <45273859+Arc-blroth@users.noreply.github.com>
2020-09-06 11:39:43 -0700

    Add contour detection test algorithm
    Signed-off-by: Arc-blroth <45273859+Arc-blroth@users.noreply.github.com>

--- TeamCode/src/main/java/org/hermitsocialclub/pandemicpanic/opmodes/ContourTestOp.java
+++ TeamCode/src/main/java/org/hermitsocialclub/pandemicpanic/opmodes/ContourTestOp.java
@@ -2,20 +2,17 @@ package org.hermitsocialclub.pandemicpanic.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.hermitsocialclub.hydra.vision.DistanceToObjectDetector;
 import org.opencv.core.Mat;
-import org.opencv.core.Point;
-import org.opencv.core.Scalar;
-import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.*;
 
-@TeleOp
-public class InternalCameraExample extends LinearOpMode {
-    OpenCvCamera phoneCam;
+@TeleOp(name = "Contour Test")
+public class ContourTestOp extends LinearOpMode {
 
     @Override
     public void runOpMode() {
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-        phoneCam = OpenCvCameraFactory.getInstance().createInternalCamera(OpenCvInternalCamera.CameraDirection.BACK, cameraMonitorViewId);
+        OpenCvCamera phoneCam = OpenCvCameraFactory.getInstance().createInternalCamera(OpenCvInternalCamera.CameraDirection.BACK, cameraMonitorViewId);
         phoneCam.openCameraDevice();
 
         phoneCam.setPipeline(new SamplePipeline());
@@ -44,22 +41,15 @@ public class InternalCameraExample extends LinearOpMode {
         }
     }
 
-    class SamplePipeline extends OpenCvPipeline {
+    private static class SamplePipeline extends OpenCvPipeline {
+
+        private final DistanceToObjectDetector distance2Obj = new DistanceToObjectDetector();
 
         @Override
         public Mat processFrame(Mat input) {
-
-            Imgproc.rectangle(
-                    input,
-                    new Point(
-                            input.cols() / 4,
-                            input.rows() / 4),
-                    new Point(
-                            input.cols() * (3f / 4f),
-                            input.rows() * (3f / 4f)),
-                    new Scalar(0, 255, 0), 4);
-
-            return input;
+            return distance2Obj.apply(input);
         }
+
     }
+
 }
