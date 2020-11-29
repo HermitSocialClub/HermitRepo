6e4bb0ed5cf25a5fe01781f371686bcfc3a17729
Arc-blroth <45273859+Arc-blroth@users.noreply.github.com>
2020-11-29 11:55:14 -0800

    Update stacc detection algorithm inputs (revision 4, based on https://gist.github.com/Arc-blroth/b091121c41543cbdb5d92043cbeca6d2)
    Signed-off-by: Arc-blroth <45273859+Arc-blroth@users.noreply.github.com>

--- TeamCode/src/main/kotlin/org/hermitsocialclub/hydra/vision/StaccDetecc.kt
+++ TeamCode/src/main/kotlin/org/hermitsocialclub/hydra/vision/StaccDetecc.kt
@@ -18,10 +18,13 @@ import kotlin.math.max
 class StaccDetecc(val config: StaccConfig = StaccConfig()) : IVisionPipelineComponent {
 
     class StaccConfig {
+        /**
+         * 300 for extremely bright light conditions and 1000 for extremely dark light conditions.
+         */
         var normUpper = 300.0
-        var lowerYellow = Scalar(19.0, 50.0, 0.0)
+        var lowerYellow = Scalar(0.0, 100.0, 100.0)
         var upperYellow = Scalar(46.0, 255.0, 255.0)
-        var maxStackRatio = 1.5
+        var maxStackRatio = Int.MAX_VALUE
     }
 
     override fun apply(image: Mat, pipeline: VisionPipeline): Mat {
@@ -54,9 +57,6 @@ class StaccDetecc(val config: StaccConfig = StaccConfig()) : IVisionPipelineComp
         val stats = Mat()
         val centroids = Mat()
         val nbComponents = connectedComponentsWithStats(filter, labels, stats, centroids, 4)
-
-        println("Stats")
-        println(stats.dump())
 
         var maxLabel: Int = -1
         var maxSize: Int = -1
