f132d7cc5a25799c5ad8868b0c56bfd44dee0ffe
Arc-blroth <45273859+arc-blroth@users.noreply.github.com>
2020-12-16 18:36:16 -0800

    Calibration suffering

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -54,9 +54,9 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 0.01926780108101677;//1.0 / rpmToVelocity(MAX_RPM);
+    public static double kV = 0.0195780108101677;//1.0 / rpmToVelocity(MAX_RPM);
     public static double kA = 0.003;
-    public static double kStatic = 0.02;
+    public static double kStatic = 0.01;
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
@@ -67,7 +67,7 @@ public class DriveConstants {
      * forces acceleration-limited profiling).
      */
     public static DriveConstraints BASE_CONSTRAINTS = new DriveConstraints(
-            30.0, 30.0, 0.0,
+            15, 15, 0.0,
             Math.toRadians(180.0), Math.toRadians(180.0), 0.0
     );
 
