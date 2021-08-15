3f9a79520ba209eaa94dc8c0b4688c337efa03f3
Cayden Pocrass <caydenrp9@gmail.com>
2021-08-15 12:50:39 -0700

    Fixed Slamra test op

--- TeamCode/src/main/java/org/hermitsocialclub/pandemicpanic/opmodes/SlamTestOp.java
+++ TeamCode/src/main/java/org/hermitsocialclub/pandemicpanic/opmodes/SlamTestOp.java
@@ -1,8 +1,12 @@
 package org.hermitsocialclub.pandemicpanic.opmodes;
 
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.canvas.Canvas;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.arcrobotics.ftclib.geometry.Pose2d;
 import com.arcrobotics.ftclib.geometry.Rotation2d;
 import com.arcrobotics.ftclib.geometry.Transform2d;
+import com.arcrobotics.ftclib.geometry.Translation2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.spartronics4915.lib.T265Camera;
@@ -12,11 +16,14 @@ import org.hermitsocialclub.telecat.PersistantTelemetry;
 @TeleOp (name = "SlamTestOp")
 public class SlamTestOp extends LinearOpMode {
 
-    Pose2d startingPose = new Pose2d(1, 1, new Rotation2d());
+    Pose2d startingPose = new Pose2d(0, 0, new Rotation2d());
 
     Transform2d cameraToRobot = new Transform2d();
 
-    double encoderMeasurementCovariance = 0.8;
+    double encoderMeasurementCovariance = 1;
+
+    private final FtcDashboard dashboard = FtcDashboard.getInstance();
+
 
     T265Camera slamra;
     T265Camera.CameraUpdate update;
@@ -27,8 +34,9 @@ public class SlamTestOp extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
-        slamra = new T265Camera(cameraToRobot, encoderMeasurementCovariance, hardwareMap.appContext);
-
+        if(slamra == null) {
+            slamra = new T265Camera(cameraToRobot, encoderMeasurementCovariance, hardwareMap.appContext);
+        }
         slamra.setPose(startingPose);
 
         slamra.start();
@@ -37,9 +45,36 @@ public class SlamTestOp extends LinearOpMode {
         waitForStart();
 
         while (opModeIsActive()) {
+            final int robotRadius = 9; // inches
+
+            TelemetryPacket packet = new TelemetryPacket();
+            Canvas field = packet.fieldOverlay();
+
             update = slamra.getLastReceivedCameraUpdate();
+            if(update == null){
+                telemetry.setDebug("slamra update", "null");
+                packet.addLine("Slamra Update Null");
+                sleep(5000);
+                break;
+            }
+            Translation2d translation = new Translation2d(update.pose.getTranslation().getX() / 0.0254, update.pose.getTranslation().getY() / 0.0254);
+            Rotation2d rotation = update.pose.getRotation();
+
+            field.strokeCircle(translation.getX(), translation.getY(), robotRadius);
+            double arrowX = rotation.getCos() * robotRadius, arrowY = rotation.getSin() * robotRadius;
+            double x1 = translation.getX() + arrowX  / 2, y1 = translation.getY() + arrowY / 2;
+            double x2 = translation.getX() + arrowX, y2 = translation.getY() + arrowY;
+            field.strokeLine(x1, y1, x2, y2);
+            packet.put("Pose", update.pose.toString());
+
+            dashboard.sendTelemetryPacket(packet);
+
             telemetry.setDebug("Pose",update.pose.toString());
+            if(isStopRequested()){
+                slamra.stop();
+            }
 
         }
+        slamra.stop();
     }
 }
