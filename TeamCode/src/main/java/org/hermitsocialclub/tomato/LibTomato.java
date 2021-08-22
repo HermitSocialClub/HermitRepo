6217f04e8d4c3de9aedff2423e55491b286b5e09
Arc-blroth <45273859+Arc-blroth@users.noreply.github.com>
2021-08-22 11:19:40 -0700

    try-catch libtomato load
    Signed-off-by: Arc-blroth <45273859+Arc-blroth@users.noreply.github.com>

--- TeamCode/src/main/java/org/hermitsocialclub/tomato/LibTomato.java
+++ TeamCode/src/main/java/org/hermitsocialclub/tomato/LibTomato.java
@@ -1,8 +1,20 @@
 package org.hermitsocialclub.tomato;
 
+import org.hermitsocialclub.util.Jukebox;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
 public class LibTomato {
     static {
-        System.loadLibrary("tomato");
+        try {
+            System.loadLibrary("tomato");
+        } catch(UnsatisfiedLinkError e) {
+            StringWriter writer = new StringWriter();
+            e.printStackTrace(new PrintWriter(writer));
+            Jukebox.INSTANCE.setTelemetryWarning(writer.toString());
+            throw new RuntimeException(e);
+        }
     }
 
     public static native int splat();
