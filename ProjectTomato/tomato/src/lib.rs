dd9f3e312dbbe7928c3ed3965ace475e199f72f6
Arc-blroth <45273859+Arc-blroth@users.noreply.github.com>
2021-10-03 17:47:41 -0700

    tflite-rs linking try 1
    Signed-off-by: Arc-blroth <45273859+Arc-blroth@users.noreply.github.com>

--- ProjectTomato/tomato/src/lib.rs
+++ ProjectTomato/tomato/src/lib.rs
@@ -11,3 +11,10 @@ use jni::JNIEnv;
 pub extern "C" fn Java_org_hermitsocialclub_tomato_LibTomato_splat(_env: JNIEnv) -> i32 {
     69
 }
+
+#[no_mangle]
+pub extern "C" fn placebo_to_force_tflite_linkage() {
+    use tflite::op_resolver::OpResolver;
+    use tflite::ops::builtin::BuiltinOpResolver;
+    println!("{:?}", BuiltinOpResolver::default().get_resolver_handle());
+}
