f4399951ebbad5a1feb0cd57f586d9ba10c1caa4
Aditya Jain <aadi4utoo@gmail.com>
2021-10-08 18:08:53 -0700

    Hide tensorflow from the children

--- ProjectTomato/tomato/src/lib.rs
+++ ProjectTomato/tomato/src/lib.rs
@@ -12,9 +12,9 @@ pub extern "C" fn Java_org_hermitsocialclub_tomato_LibTomato_splat(_env: JNIEnv)
     69
 }
 
-#[no_mangle]
-pub extern "C" fn placebo_to_force_tflite_linkage() {
-    use tflite::op_resolver::OpResolver;
-    use tflite::ops::builtin::BuiltinOpResolver;
-    println!("{:?}", BuiltinOpResolver::default().get_resolver_handle());
-}
+// #[no_mangle]
+// pub extern "C" fn placebo_to_force_tflite_linkage() {
+//     use tflite::op_resolver::OpResolver;
+//     use tflite::ops::builtin::BuiltinOpResolver;
+//     println!("{:?}", BuiltinOpResolver::default().get_resolver_handle());
+// }
