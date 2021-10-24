3b08b25b0e32db9a68c3e04fa79510d55667d093
Arc-blroth <45273859+Arc-blroth@users.noreply.github.com>
2021-10-24 10:48:00 -0700

    make it not crash hopefully
    Signed-off-by: Arc-blroth <45273859+Arc-blroth@users.noreply.github.com>

--- TeamCode/src/main/kotlin/org/hermitsocialclub/tomato/Midas.kt
+++ TeamCode/src/main/kotlin/org/hermitsocialclub/tomato/Midas.kt
@@ -1,19 +1,17 @@
 package org.hermitsocialclub.tomato
 
 import android.os.Environment
-import com.qualcomm.robotcore.util.RobotLog
 import org.hermitsocialclub.hydra.vision.IVisionPipelineComponent
 import org.hermitsocialclub.hydra.vision.VisionPipeline
-import org.hermitsocialclub.hydra.vision.util.asByteBuffer
 import org.opencv.core.CvType
 import org.opencv.core.Mat
 import org.opencv.imgproc.Imgproc
 import org.opencv.imgproc.Imgproc.cvtColor
+import org.opencv.imgproc.Imgproc.resize
 import org.tensorflow.lite.DataType
 import org.tensorflow.lite.Interpreter
 import org.tensorflow.lite.gpu.CompatibilityList
 import org.tensorflow.lite.gpu.GpuDelegate
-import org.tensorflow.lite.support.common.ops.NormalizeOp
 import org.tensorflow.lite.support.image.ImageProcessor
 import org.tensorflow.lite.support.image.TensorImage
 import org.tensorflow.lite.support.image.ops.ResizeOp
@@ -21,7 +19,6 @@ import org.tensorflow.lite.support.image.ops.ResizeOp.ResizeMethod
 import org.tensorflow.lite.support.image.ops.ResizeWithCropOrPadOp
 import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
 import java.io.File
-import java.lang.IllegalArgumentException
 import java.nio.ByteBuffer
 import kotlin.math.min
 
@@ -45,6 +42,7 @@ class Midas : AutoCloseable, IVisionPipelineComponent {
     private val imageSizeY: Int
     private val inputImage: TensorImage
     private val outputProbability: TensorBuffer
+    private var badHack: ByteArray? = null
 
     init {
         @Suppress("DEPRECATION")
@@ -79,36 +77,38 @@ class Midas : AutoCloseable, IVisionPipelineComponent {
 
     override fun apply(mat: Mat, pipeline: VisionPipeline): Mat {
         // Load image
+        val inputSize = mat.size()
         val bgrMat = Mat()
-        pipeline.telemetry.setData(TAG, "cvtColor")
         cvtColor(mat, bgrMat, Imgproc.COLOR_RGBA2BGR)
-        pipeline.telemetry.setData(TAG, "TensorBuffer.createFixedSize")
         val inputBuffer = TensorBuffer.createFixedSize(intArrayOf(mat.height(), mat.width(), 3), DataType.UINT8)
-        pipeline.telemetry.setData(TAG, "badHackPlsRemove")
-        val badHackPlsRemoveFloatBuffer = ByteArray((bgrMat.total() * bgrMat.channels()).toInt())
-        bgrMat.get(0, 0, badHackPlsRemoveFloatBuffer)
-        pipeline.telemetry.setData(TAG, "loadBuffer")
-        inputBuffer.loadBuffer(ByteBuffer.wrap(badHackPlsRemoveFloatBuffer))
-        pipeline.telemetry.setData(TAG, "load")
+        val inputSizeInBytes = (mat.total() * mat.channels()).toInt()
+        val badHackBuf = if(this.badHack?.size == inputSizeInBytes) {
+            this.badHack!!
+        } else {
+            val newBuf = ByteArray(inputSizeInBytes)
+            this.badHack = newBuf
+            newBuf
+        }
+        bgrMat.get(0, 0, badHackBuf)
+        inputBuffer.loadBuffer(ByteBuffer.wrap(badHackBuf))
         inputImage.load(inputBuffer)
 
         // Process image
         // NormalizeOp arguments are from https://git.io/JKikt
         val cropSize = min(mat.width(), mat.height())
-        pipeline.telemetry.setData(TAG, "resize and normalize")
         ImageProcessor.Builder()
             .add(ResizeWithCropOrPadOp(cropSize, cropSize))
             .add(ResizeOp(imageSizeX, imageSizeY, ResizeMethod.NEAREST_NEIGHBOR))
-            .add(NormalizeOp(115.0f, 58.0f))
+        //  .add(NormalizeOp(115.0f, 58.0f))
             .build()
             .process(inputImage)
 
         // Run model!
-        pipeline.telemetry.setData(TAG, "interpreter.run")
         interpreter.run(inputImage.buffer, outputProbability.buffer.rewind())
 
-        val outBgrMat = Mat(imageSizeY, imageSizeX, CvType.CV_8UC(3), outputProbability.buffer)
-        pipeline.telemetry.setData(TAG, "cvtColor 2")
+        val modelOutMat = Mat(imageSizeY, imageSizeX, CvType.CV_8UC(3), outputProbability.buffer)
+        val outBgrMat = Mat()
+        resize(modelOutMat, outBgrMat, inputSize)
         cvtColor(outBgrMat, mat, Imgproc.COLOR_BGR2RGBA)
         return mat
     }
